a1c1ee00ca85d436f017ae4943560e6e2d428fcd
ExtraRandom <extrarandom@users.noreply.github.com>
2026-05-18 11:45:05 +0100

    removed some old commands that aren't needed

--- cogs/fun.py
+++ cogs/fun.py
@@ -7,25 +7,6 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name="8ball")
-    async def eight_ball(self, ctx, question: discord.Option(str, "Question for the Eight Ball", required=False)):
-        """It's a ~magic~ 8 ball"""
-        answers = [
-            # Affirmative answers (10)
-            "Yes - definitely", "Yes", "Most likely", "Signs point to yes", "As I see it, yes",
-            "It is certain", "It is decidedly so", "Without a doubt", "You may rely on it", "Outlook good" 
-            
-            # Non-committal answers (5) 
-            "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now",
-            "Concentrate and ask again",
-
-            # Negative answers (5)
-            "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"
-        ]
-        random.seed()
-        response = "**My Response is:** {}".format(random.choice(answers))
-        await ctx.respond(response)
-
     @commands.slash_command(
         name="pick",
         integration_types={
