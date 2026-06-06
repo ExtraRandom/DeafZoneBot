af4d07f43a1d959088edad9d208aa4560e8a2f02
ExtraRandom <extrarandom@users.noreply.github.com>
2026-06-06 07:24:23 +0100

    added reject command

--- cogs/verification.py
+++ cogs/verification.py
@@ -93,5 +93,78 @@ class Verification(commands.Cog):
         # send a message in thread with "user approved" user id, and their original intro
         await ctx.respond(f"User Approved\n\nUser ID: {user.id}\n\nIntro: {msg.content}")
 
+    @commands.slash_command(
+        name="reject",
+        default_member_permissions=discord.Permissions(manage_roles=True),
+        # only roles with manage role perm can see/use
+    )
+    async def verification_reject(
+            self, ctx: discord.ApplicationContext,
+            reason: discord.Option(str, description="reason", default="No Reason Provided")
+    ):
+        await ctx.defer()
+
+        # get the channel this was used in
+        chan = ctx.channel
+        if type(chan) != discord.Thread:
+            await ctx.respond(f"This command only works in a thread", ephemeral=True)
+            return
+
+        # get starter message and author
+        try:
+            msg = await chan.parent.fetch_message(chan.id)
+            user = msg.author
+        except discord.NotFound:
+            await ctx.respond(f"Could not find starting message, will not proceed")
+            return
+
+        # check starter message is in introduction channel
+        if msg.channel.id != self.intro_channel:
+            await ctx.respond("This command needs to used in a thread in the introductions channel")
+            return
+
+        # check user isn't already approved (has member role) and check user doesn't have rejection role
+        user_has_quarantine_role = False
+        q_role = None
+
+        roles = user.roles
+        for role in roles:
+            if role.id == self.approved_role:
+                await ctx.respond(f"User {user.name} is already approved")
+                return
+            elif role.id == self.rejected_role:
+                await ctx.respond(f"User {user.name} already has the rejected role")
+                return
+            #elif role.id == self.quarantine_role:
+            #    user_has_quarantine_role = True
+            #    q_role = role
+
+            # print(role.id, role.name)
+
+        # make sure bot has the manage roles permission
+        bot_member = ctx.guild.me
+        if not bot_member.guild_permissions.manage_roles:
+            await ctx.respond(f"The bot does not have manage roles permission and cannot proceed")
+            return
+
+        # add reject role
+        reject = await ctx.guild.fetch_role(self.rejected_role)
+        await user.add_roles(reject)
+
+        # make sure reason isn't blank
+        if len(reason) == 0:
+            reason = "No Reason Provided"
+
+        # log in a channel (probably a staff related one)
+        staff_channel = await ctx.guild.fetch_channel(self.log_channel)
+        embed = discord.Embed(title="User Rejected", color=discord.Color.green())
+        embed.add_field(name="User", value=user.mention)
+        embed.add_field(name="User ID", value=str(user.id))
+        embed.add_field(name="Reason", value=reason)
+        await staff_channel.send(embed=embed)
+
+        # send a message in thread with "user rejected" user id, and their original intro
+        await ctx.respond(f"User Rejected\n\nReason: {reason}\n\nUser ID: {user.id}\n\nIntro: {msg.content}")
+
 def setup(bot):
     bot.add_cog(Verification(bot))
